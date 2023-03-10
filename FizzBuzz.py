80c16cfe324bebd7ac9831bb32a443bd135a311e
Nicholas Kapp <nic@Nicholass-MacBook-Air-2.local>
2023-03-10 16:24:59 -0600

    branch03

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,11 @@
 for i in range(1, 101):
-    output = ""
-    if i % 3 == 0:
-        output += "Fizz"
-    if i % 5 == 0:
-        output += "Buzz"
-    if output == "":
-        output = i
-    print(output)
+    fizz = i % 3 == 0
+    buzz = i % 5 == 0
+    if fizz and buzz:
+        print("FizzBuzz")
+    elif fizz:
+        print("Fizz")
+    elif buzz:
+        print("Buzz")
+    else:
+        print(i)
